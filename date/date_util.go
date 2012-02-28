f2ecf519dd6d16171cd17707e80c8bde13b2f5ef
korbenzhang <korbenzhang@gmail.com>
2012-02-28 10:57:08 +0800

    add date

---
+++ date/date_util.go
@@ -0,0 +1,14 @@
+package date
+
+
+import (
+	"fmt"
+)
+
+
+func Sign(){	
+	fmt.Println("Call from mabetle-go-utils date.Sign()")
+}
+
+
+
---
+++ string/string_case.go
@@ -0,0 +1,17 @@
+package string
+
+
+import (
+	"fmt"
+)
+
+
+
+func UpperCase(){
+	fmt.Println("To UpperCase")	
+}
+
+func LowerCase(){
+	fmt.Println("To LowerCase")
+}
+
